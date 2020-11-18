d5026d6e823f47fed5dc532cd2e4133e56e6a625
Patryk Śledź <66391813+patryk0504@users.noreply.github.com>
2020-11-18 18:25:13 +0100

    Final solution lab05

--- lab05/src/Task1.scala
+++ lab05/src/Task1.scala
@@ -1,3 +1,56 @@
 object Task1 {
+  def main(args: Array[String]): Unit = {
+
+    val firstMcSet = new NormalMcSet with BigMac with Fries with Coke
+    val firstMcSetLarge = new LargeMcSet with BigMac with Fries with Coke
+    val secondMcSet = new LargeMcSet with McWrap with Fries
+
+    println(firstMcSet.name + " " + firstMcSet.price)
+    println(firstMcSetLarge.name + " " + firstMcSetLarge.price)
+    println(secondMcSet.name + " " + secondMcSet.price)
+  }
+
+  trait Set{
+    def name() : String = {"McSet with "}
+    def price() : Double
+    val largeOrNormal : String
+    val newPrice : Double
+  }
+
+  class NormalMcSet extends Set{
+    override def name() : String = {super.name()}
+    override def price(): Double = {0}
+    override val largeOrNormal: String = "normal"
+    override val newPrice: Double = 1.0
+  }
+
+  class LargeMcSet extends Set{
+    override def name() : String = {super.name()}
+    override def price() : Double = {0}
+    override val largeOrNormal: String = "large"
+    override val newPrice: Double = 1.5
+  }
+
+  trait BigMac extends Set{
+    abstract override def name() : String= {super.name() + " BigMac "}
+    abstract override def price(): Double = {10.0}
+  }
+
+  trait McWrap extends Set{
+    abstract override def name() : String = {super.name() + " McWrap "}
+    abstract override def price(): Double = { 9.0}
+  }
+
+  trait Fries extends Set {
+    abstract override def name() : String= {super.name() + largeOrNormal + " fries " }
+    abstract override def price(): Double = {super.price()  + newPrice * 5.0}
+  }
+
+  trait Coke extends Set{
+    abstract override def name() : String= {super.name() + largeOrNormal + " coke " }
+    abstract override def price(): Double = {super.price() + newPrice * 4.0}
+  }
+
+
 
 }
--- lab05/src/Task2.scala
+++ lab05/src/Task2.scala
@@ -1,3 +1,39 @@
 object Task2 {
+  def main(args: Array[String]):Unit = {
 
+    val s = new EquationSolver()
+    println(s)
+    s << Addition(1) << Subtraction(1)
+    println(s)
+    s << Division(0)
+    println(s)
+    s << Addition(1) << Multiplication(5)
+    println(s)
+    s << Division(2)
+    println(s)
+    s << List[Int](1)
+    println(s)
+  }
 }
+
+
+class EquationSolver(var result : Double = 0.0){
+  def <<(eqObj: AnyRef): EquationSolver = {
+    eqObj match {
+      case add : Addition => result = result + add.x; println("Adding " + add.x)
+      case sub : Subtraction => result = result - sub.x; println("Subtracting " + sub.x)
+      case div : Division if div.x != 0.0 => result = result / div.x; println("Dividing by " + div.x)
+      case mul : Multiplication => result = result * mul.x; println("Multiplying by " + mul.x)
+      case _ => println("Unknown / not allowed operation")
+    }
+    this
+  }
+  override def toString: String = {"Result: " + result.toString}
+}
+
+
+abstract class Equation
+case class Addition(x : Double) extends Equation
+case class Subtraction(x : Double) extends Equation
+case class Division(x : Double) extends Equation
+case class Multiplication(x : Double) extends Equation
